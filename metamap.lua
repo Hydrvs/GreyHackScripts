1df5b0d6e0baeee9941d6e52082950c0c9532876
Hydrvs <Hydrvs@users.noreply.github.com>
2019-05-05 15:59:25 -0500

    Update metamap.lua

--- metamap.lua
+++ metamap.lua
@@ -23,17 +23,13 @@ port = params[1].to_int
 net_session = metaxploit.net_use( address, port)
 if not net_session then exit("Error: can't connect to net session")
 metaLib = net_session.dump_libs
-
-// payload1
-result = metaLib.overflow(memoryzone, payload1, "pass")
-if not result then print("Scanned payload: " + memoryzone + " " + payload1)
-if typeof(result) == "shell" then
-	result.start_terminal
-else
-	if typeof(result) == "port" then print("Obtained port: " + result)
-	if typeof(result) == "string" then print("Obtained string: " + result)
-	if typeof(result) == "router" then print("Obtained router: " + result)
-	if typeof(result) == "computer" then print("Obtained computer: " + result)
+			
+//Function
+res = function(x)			
+	if typeof(result) == "port" then print("Obtained port: " + result) return x
+	if typeof(result) == "string" then print("Obtained string: " + result) return x
+	if typeof(result) == "router" then print("Obtained router: " + result) return x
+	if typeof(result) == "computer" then print("Obtained computer: " + result) return x
 	if typeof(result) == "file" then
 		print("Obtained Folder: "+result.path)
 		subFiles = result.get_folders + result.get_files
@@ -43,7 +39,17 @@ else
 			data = data + "\n" + "owner: " + result.owner + "\n" + "permissions: " + result.permissions
 			print(format_columns(data))		
 		end for
+		return x
 	end if
+end function
+
+// payload1
+result = metaLib.overflow(memoryzone, payload1, "pass")
+if not result then print("Scanned payload: " + memoryzone + " " + payload1)
+if typeof(result) == "shell" then
+	result.start_terminal
+else
+	res(result)
 end if
 print(" ")
 //payload2
@@ -52,20 +58,7 @@ if not result then print("Scanned payload: " + memoryzone + " " + payload2)
 if typeof(result) == "shell" then
 	result.start_terminal
 else
-	if typeof(result) == "port" then print("Obtained port: " + result)
-	if typeof(result) == "string" then print("Obtained string: " + result)
-	if typeof(result) == "router" then print("Obtained router: " + result)
-	if typeof(result) == "computer" then print("Obtained computer: " + result)
-	if typeof(result) == "file" then
-		print("Obtained Folder: "+result.path)
-		subFiles = result.get_folders + result.get_files
-		for subFile in subFiles
-			nameFile = subFile.name
-			data = "file: " + nameFile 
-			data = data + "\n" + "owner: " + result.owner + "\n" + "permissions: " + result.permissions
-			print(format_columns(data))		
-		end for
-	end if
+	res(result)
 end if
 print(" ")
 //payload3
@@ -74,20 +67,7 @@ if not result then print("Scanned payload: " + memoryzone + " " + payload3)
 if typeof(result) == "shell" then
 	result.start_terminal
 else
-	if typeof(result) == "port" then print("Obtained port: " + result)
-	if typeof(result) == "string" then print("Obtained string: " + result)
-	if typeof(result) == "router" then print("Obtained router: " + result)
-	if typeof(result) == "computer" then print("Obtained computer: " + result)
-	if typeof(result) == "file" then
-		print("Obtained Folder: "+result.path)
-		subFiles = result.get_folders + result.get_files
-		for subFile in subFiles
-			nameFile = subFile.name
-			data = "file: " + nameFile 
-			data = data + "\n" + "owner: " + result.owner + "\n" + "permissions: " + result.permissions
-			print(format_columns(data))		
-		end for
-	end if
+	res(result)
 end if
 print(" ")
 //payload4
@@ -96,21 +76,7 @@ if not result then print("Scanned payload: " + memoryzone + " " + payload4)
 if typeof(result) == "shell" then
 	result.start_terminal
 else
-	if typeof(result) == "port" then print("Obtained port: " + result)
-	if typeof(result) == "string" then print("Obtained string: " + result)
-	if typeof(result) == "router" then print("Obtained router: " + result)
-	if typeof(result) == "computer" then print("Obtained computer: " + result)
-	if typeof(result) == "file" then
-		print("Obtained Folder: "+result.path)
-		print(" ")
-		subFiles = result.get_folders + result.get_files
-		for subFile in subFiles
-			nameFile = subFile.name
-			data = "file: " + nameFile 
-			data = data + "\n" + "owner: " + result.owner + "\n" + "permissions: " + result.permissions
-			print(format_columns(data))		
-		end for
-	end if
+	res(result)
 end if
 print(" ")
 //payload5
@@ -119,21 +85,7 @@ if not result then print("Scanned payload: " + memoryzone + " " + payload5)
 if typeof(result) == "shell" then
 	result.start_terminal
 else
-	if typeof(result) == "port" then print("Obtained port: " + result)
-	if typeof(result) == "string" then print("Obtained string: " + result)
-	if typeof(result) == "router" then print("Obtained router: " + result)
-	if typeof(result) == "computer" then print("Obtained computer: " + result)
-	if typeof(result) == "file" then
-		print("Obtained Folder: "+result.path)
-		print(" ")
-		subFiles = result.get_folders + result.get_files
-		for subFile in subFiles
-			nameFile = subFile.name
-			data = "file: " + nameFile 
-			data = data + "\n" + "owner: " + result.owner + "\n" + "permissions: " + result.permissions
-			print(format_columns(data))		
-		end for
-	end if
+	res(result)
 end if
 print(" ")
 //payload6
@@ -142,35 +94,5 @@ if not result then print("Scanned payload: " + memoryzone + " " + payload6)
 if typeof(result) == "shell" then
 	result.start_terminal
 else
-	if typeof(result) == "port" then print("Obtained port: " + result)
-	if typeof(result) == "string" then print("Obtained string: " + result)
-	if typeof(result) == "router" then print("Obtained router: " + result)
-	if typeof(result) == "computer" then print("Obtained computer: " + result)
-	if typeof(result) == "file" then
-		print("Obtained Folder: "+result.path)
-		print(" ")
-		subFiles = result.get_folders + result.get_files
-		for subFile in subFiles
-			nameFile = subFile.name
-			data = "file: " + nameFile 
-			data = data + "\n" + "owner: " + result.owner + "\n" + "permissions: " + result.permissions
-			print(format_columns(data))		
-		end for
-	end if
+	res(result)
 end if
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
